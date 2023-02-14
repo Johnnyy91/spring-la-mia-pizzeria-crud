5a4740b10dab0512e4a67366d224357a0648534d
Johnnyy91 <claudiosardilli@gmail.com>
2023-02-14 17:54:59 +0100

    modification file

--- src/main/java/com/example/pizzeria/controller/PizzaController.java
+++ src/main/java/com/example/pizzeria/controller/PizzaController.java
@@ -14,20 +14,12 @@ import com.example.pizzeria.repository.PizzaRepository;
 @Controller
 @RequestMapping("/")
 public class PizzaController {
-	/*
-	 * 1. creare da dbeaver almeno 1 pizza  ok
-	 * 2. scrivere il codice thymeleaf in index.html per visualizzare le pizze  ok
-	 *   - come primo step visualizzo semplicemente <p th:text="$pizze"></p> ---> [org.zzasdasd.Pizza237823723]  ok 
-	 *   - come secondo step, le visualizzo per bene facendo th:each
-	 *   
-	 */
+	
 	
 	@GetMapping()
 	public String home(Model model) {
 		
-		//somma(1,3)
-		//int c = somma (1,3)
-		//System.out.print(c);
+	
 		
 		List<Pizza> listPizza = pizzarepository.findAll();
 		
