90e9735d45325fde5e8c66efc1b418ec2f4cc318
Johnnyy91 <claudiosardilli@gmail.com>
2023-02-15 17:27:05 +0100

    add details pizza

--- src/main/java/com/example/pizzeria/controller/PizzaController.java
+++ src/main/java/com/example/pizzeria/controller/PizzaController.java
@@ -1,13 +1,14 @@
 package com.example.pizzeria.controller;
 
 import java.util.List;
-
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import com.example.pizzeria.model.Pizza;
 import com.example.pizzeria.repository.PizzaRepository;
 
@@ -17,18 +18,47 @@ public class PizzaController {
 	
 	
 	@GetMapping()
-	public String home(Model model) {
+	public String home(@RequestParam(name="keyword", required = false) String keyword, Model model) {
 		
-	
+		List<Pizza> listPizza;
 		
-		List<Pizza> listPizza = pizzarepository.findAll();
-		
-		model.addAttribute("name" , "Claudio");
+		if (keyword!=null && !keyword.isEmpty()) {
+			
+			 listPizza = pizzarepository.findByNameLike("%" + keyword + "%");
+			
+		} else {
+			
+			 listPizza = pizzarepository.findAll();
+		}
 		model.addAttribute("pizze", listPizza);
 		return "index";
 	}
 	
 	 @Autowired
 	 PizzaRepository pizzarepository;
+	 
+	 
+	 @GetMapping("/pizza/{id}")
+	 public String detail (@PathVariable("id") Integer id , Model model ) {
+		 
+		 Optional<Pizza> k = pizzarepository.findById(id);
+		 
+		 if (k.isPresent()) {
+			 model.addAttribute("pizza", k.get());
+		 } else {
+			 return "404";
+		 }
+		 
+		 return "/detail";
+	 }
+	 
+	 /*
+	 @GetMapping("/search")
+		public String home(@RequestParam(name="keyword", required = false) String keyword, Model model) {
+			
+			List<Pizza> listPizza = pizzarepository.findByNameLike("%" + keyword + "%");
+			model.addAttribute("pizze", listPizza);
+			return "index";
+		} */
 
 }
